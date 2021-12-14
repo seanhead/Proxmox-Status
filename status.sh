86c47ffe7e49c77949ea1ffda4900b19a80bc033
sean head <seanh@head-net.com>
2021-12-14 13:14:54 -0800

    Update status.sh
    Shellcheck

--- status.sh
+++ status.sh
@@ -5,20 +5,19 @@ sudo echo 0 > /dev/null
 
 screenfetch
 
-dir=$(dirname $(realpath "$0"))
+dir=$(dirname "$(realpath "$0")")
 
 printf "\nCPU Load / Temperature:\n"
-$dir/scripts/cpuinfo.sh | sed 's/^/\t/'
+"$dir"/scripts/cpuinfo.sh | sed 's/^/\t/'
 
 printf "\nWD Red HDD Temperatures:\n"
-$dir/scripts/hddtemp.sh | sed 's/^/\t/'
+"$dir"/scripts/hddtemp.sh | sed 's/^/\t/'
 
 printf "\nSSD Temperatures:\n"
-$dir/scripts/ssdtemp.sh | sed 's/^/\t/'
+"$dir"/scripts/ssdtemp.sh | sed 's/^/\t/'
 
 printf "\nIntel Optane SLOG Temperatures:\n"
-$dir/scripts/optanetemp.sh | sed 's/^/\t/'
+"$dir"/scripts/optanetemp.sh | sed 's/^/\t/'
 
 printf "\nZFS Adaptive Read Cache Stats:\n"
-$dir/scripts/arcstats.sh | sed 's/^/\t/'
-
+"$dir"/scripts/arcstats.sh | sed 's/^/\t/'
